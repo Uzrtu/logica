5293053fd810f1df1e3b4a213447401a3a23c155
Uzrtu <00001129883007xsp@al.educacao.sp.gov.br>
2026-08-18 12:28:59 -0300

    em aula

--- semana-15/for.py
+++ semana-15/for.py
@@ -8,5 +8,11 @@ for i in range(6):
     print(i)
 '''
 
-for i in range(0, 10, 2):
+'''
+for i in range(0, 12, 2):
     print(i)
+'''
+for i in range(10, -1, -1):
+    print(i)
+else:
+    print("Lançar!")
