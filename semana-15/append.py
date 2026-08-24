ead48382efab84c588726426ce910a1f1912583e
Uzrtu <00001129883007xsp@al.educacao.sp.gov.br>
2026-08-24 10:36:29 -0300

    em aula

---
+++ semana-15/append.py
@@ -0,0 +1,16 @@
+'''
+lista = [7.5, 8.0, 6.5, 9.0, 7.0]
+print(lista)
+'''
+'''
+numeros = []
+numeros.append(1)
+numeros.append(2)
+numeros.append(3)
+numeros.append(4)
+numeros.append(5)
+print(numeros)
+'''
+
+numeros = [x for x in range(1, 6)]
+print(numeros)
