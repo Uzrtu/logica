11e3fb7e6c1c01411181fe10c518acd88ed36ea8
Uzrtu <00001129883007xsp@al.educacao.sp.gov.br>
2026-08-27 12:18:16 -0300

    em aula

--- semana-15/temperatura.py
+++ semana-15/temperatura.py
@@ -1,3 +1,12 @@
 temperaturas = [22, 25, 21, 24, 26, 23, 22]
 
 print(f"Na lista tem {len(temperaturas)} temperaturas")
+
+
+print(f"{temperaturas[0]} tem o indice = {temperaturas.index(22)}")
+print(f"{temperaturas[6]} tem o indice = {temperaturas.index(20)}")
+
+i = 0
+while i < len(temperaturas):
+    print(temperaturas[i])
+    i += 1
