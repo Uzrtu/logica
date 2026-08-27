b9191ed51ce259fa6368e01f8a3157e1dc8e5376
Uzrtu <00001129883007xsp@al.educacao.sp.gov.br>
2026-08-27 11:39:21 -0300

    em aula

---
+++ semana-15/tarefa.py
@@ -0,0 +1,12 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10]
+#lista = [x for x in range(1, 11)]
+'''
+for i in range(0, len(lista)):
+    print(lista[i])
+'''
+#print(lista[3])
+
+frutas = ["maçã", "banana", "pera"]
+#for fruta in frutas:
+    #print(fruta)
+print(frutas[2])
---
+++ semana-15/temperatura.py
@@ -0,0 +1,3 @@
+temperaturas = [22, 25, 21, 24, 26, 23, 22]
+
+print(f"Na lista tem {len(temperaturas)} temperaturas")
